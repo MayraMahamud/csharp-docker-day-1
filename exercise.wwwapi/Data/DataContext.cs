9c1310834d4733751b39e01672294637bbe460f4
MMaha1 <mayraam049@gmail.com>
2024-09-17 15:19:29 +0200

    ;)

--- exercise.wwwapi/Data/DataContext.cs
+++ exercise.wwwapi/Data/DataContext.cs
@@ -5,12 +5,11 @@ namespace exercise.wwwapi.Data
 {
     public class DataContext : DbContext
     {
-        private string _connectionString;
+       
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            _connectionString = configuration.GetValue<string>("ConnectionStrings:DockerPostgres")!;
+           
 
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
